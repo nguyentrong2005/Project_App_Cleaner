2b6665426bd304dbb1dfd8e346f376d2214b0282
ThanhTrinh31 <huynhthanhtrinh2812@gmail.com>
2025-05-30 10:05:45 +0700

    Tích hợp điều khoản người dùng

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # main.py
 from core import run_scan
 from gui import run_main_window
+from LicenseScreen.license_agreement import show_license_screen
 
+def start_main_app():
+    window.mainloop()
+    
 if __name__ == "__main__":
-    run_main_window()
+    show_license_screen(on_accept=start_main_app)
     # run_scan()
