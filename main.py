46634272bf6a475aeaa18153377106543e09e9fe
ThanhTrinh31 <huynhthanhtrinh2812@gmail.com>
2025-06-04 08:55:22 +0700

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gui import run_main_window
 from LicenseScreen.license_agreement import show_license_screen
 
 def start_main_app():
-    window.mainloop()
+    run_main_window()
     
 if __name__ == "__main__":
     show_license_screen(on_accept=start_main_app)
